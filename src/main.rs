d1e99f542f053cc8348d585ceca35ce575dabb8b
Damezuma <qweads12@gmail.com>
2020-03-19 07:42:16 +0900

    change bmp to png

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,30 @@
 extern crate aom_av1_sys;
 extern crate avif_parser;
-extern crate bmp;
+extern crate png;
 extern crate cvt_yuv_rgb;
 use std::fs::File;
-use std::io::Cursor;
-use std::io::Read;
+use std::io::{Read, Cursor, Write};
 use aom_av1_sys::decode::Av1Decoder;
 use cvt_yuv_rgb::*;
+fn map_err<T:std::fmt::Debug>(err:T){
+    eprintln!("{:?}", err);
+}
 fn main()->Result<(), ()>{
+
     let mut avif_file = File::open("./hato.profile0.8bpc.yuv420.avif").unwrap();
     let mut avif_data  = Vec::new();
-    avif_file.read_to_end(&mut avif_data).map_err(|err|{ eprintln!("{:?}", err); return ();})?;
+    avif_file.read_to_end(&mut avif_data).map_err(map_err)?;
     let avif = avif_parser::parse(Cursor::new(&mut avif_data))?;
     println!("{:?}", avif);
-    let mut decoder:Av1Decoder = Av1Decoder::new().map_err(|err|{
-        eprintln!("{:?}", err);
-        return ();
-    })?;
+    let mut decoder:Av1Decoder = Av1Decoder::new().map_err(map_err)?;
     if let Some(item) = avif.media_boxes().first(){
         let tmp = &avif_data.as_slice()[item.0 as usize .. (item.0 + item.1) as usize];
-        eprintln!("{}\n{}\n{}", tmp.len(), item.1, item.1 == tmp.len() as u32);
-        decoder.decode(tmp).map_err(|err|{
-            eprintln!("{:?}", err);
-            return ();
-        })?;
+        decoder.decode(tmp).map_err(map_err)?;
         let image = decoder.get_frame().ok_or(())?;
-        let mut bmp_image = bmp::Image::new(image.d_width(), image.d_height());
+        let mut file = File::create("./result.png").map_err(map_err)?;
+        let mut png_image = png::Encoder::new(file, image.d_width(), image.d_height());
+        png_image.set_color(png::ColorType::RGB);
+        png_image.set_depth(png::BitDepth::Eight);
         let yuv = YUVImageBuilder::new()
         .plane_y(image.y_plane(), image.y_stride())
         .plane_u(image.u_plane(), image.u_stride())
@@ -44,17 +43,14 @@ fn main()->Result<(), ()>{
                 unimplemented!();
             }
         };
+        let mut writer = png_image.write_header().map_err(map_err)?;
+        let mut stream_writer = writer.stream_writer();
         for (index, color) in RGB24Writer::new(YUV2RGB::new(yuv.iter(), color_prime)).enumerate(){
             let index= index as u32;
-            let pixel = bmp::Pixel::new(color[0],color[1],color[2]);
-            bmp_image.set_pixel(index % image.d_width(), index / image.d_width(),  pixel);
+            stream_writer.write(&color).map_err(map_err)?;
         }
-        bmp_image.save("./test-result.bmp").map_err(|err|{
-            eprintln!("{:?}", err);
-            return ();
-        });
+        stream_writer.finish().map_err(map_err)?;
+        std::mem::drop(writer);
     }
-    
-    println!("Hello, world!");
     return Ok(());
 }
