d3523918b6b523e0109d5372e5ad8d4259acc5bc
Damezuma <qweads12@gmail.com>
2020-03-19 07:21:57 +0900

    complete first test

--- src/main.rs
+++ src/main.rs
@@ -1,86 +1,60 @@
-extern crate libc;
-
-// #[derive(PartialEq)]
-// #[repr(C)]
-// enum aom_codec_err_t{
-//     AOM_CODEC_OK,
-//     AOM_CODEC_ERROR,
-//     AOM_CODEC_MEM_ERROR,
-//     AOM_CODEC_ABI_MISMATCH,
-//     AOM_CODEC_INCAPABLE,
-//     AOM_CODEC_UNSUP_BITSTREAM,
-//     AOM_CODEC_UNSUP_FEATURE,
-//     AOM_CODEC_CORRUPT_FRAME,
-//     AOM_CODEC_INVALID_PARAM,
-//     AOM_CODEC_LIST_END
-// }
-// #[repr(C)]
-// struct aom_codec_ctx{
-//     name:*const std::os::raw::c_char,
-//     iface:pvoid,
-//     err:aom_codec_err_t,
-//     err_detail:*const  std::os::raw::c_char,
-//     config:pvoid,
-//     private:pvoid
-// }
-// type void = std::os::raw::c_void;
-// type pvoid = *const void;
-// type long = libc::c_long;
-// type int = libc::c_int;
-// type aom_codec_iter_t = *const void;
-// const AOM_IMAGE_ABI_VERSION:i32 = 6;
-// const AOM_CODEC_ABI_VERSION:i32 = AOM_IMAGE_ABI_VERSION + 3;
-// const AOM_DECODER_ABI_VERSION:i32 = AOM_CODEC_ABI_VERSION + 4;
-// #[link(name="aom", kind="static")]
-// extern{
-//     fn aom_codec_av1_dx()-> pvoid;
-//     fn aom_img_free(img: pvoid);
-//     fn aom_codec_destroy(codec:&mut aom_codec_ctx);
-//     fn aom_codec_decode(codec:&mut aom_codec_ctx, data:*const u8, len: libc::size_t, _:pvoid)->aom_codec_err_t;
-//     fn aom_codec_get_frame(codec:&mut aom_codec_ctx, iter: *const aom_codec_iter_t)->pvoid;
-//     fn aom_codec_dec_init_ver(ctx:&mut aom_codec_ctx, iface:pvoid, cfg:pvoid, flags:long, ver:int)->aom_codec_err_t;
-// }
-
-// struct Av1Decoder
-// {
-//     decode_context:aom_codec_ctx
-// }
-// use std::mem::MaybeUninit;
-// impl Av1Decoder{
-//     fn new()->Result<Self,aom_codec_err_t>{
-//         unsafe{
-//             let decoder_interface = aom_codec_av1_dx();
-//             let mut decode_context:aom_codec_ctx = MaybeUninit::zeroed().assume_init();
-//             let res = aom_codec_dec_init_ver(&mut decode_context, decoder_interface, std::ptr::null(), 0, AOM_DECODER_ABI_VERSION);
-//             if res != aom_codec_err_t::AOM_CODEC_OK {
-//                 return Err(res);
-//             }
-//             Ok(Self{
-//                 decode_context:decode_context
-//             })
-//         }
-//     }
-//     fn decode(&mut self, data:&[u8])->Result<(), aom_codec_err_t>{
-//         unsafe{
-//             let res = aom_codec_decode(&mut self.decode_context, data.as_ptr(), data.len(), std::ptr::null());
-//             if res != aom_codec_err_t::AOM_CODEC_OK{
-//                 return Err(res);
-//             }
-//             return Ok(());
-//         }
-//     }
-//     fn get_frame(&mut self)->Option<()>{
-//         let mut s:aom_codec_iter_t = std::ptr::null();
-//         unsafe{
-//             let res = aom_codec_get_frame(&mut self.decode_context, &s);
-//             if res == std::ptr::null(){
-//                 return None;
-//             }
-//         }
-//         return None;
-//     }
-// }
-fn main() {
-    let decoder =unsafe{aom_codec_av1_dx()};
+extern crate aom_av1_sys;
+extern crate avif_parser;
+extern crate bmp;
+extern crate cvt_yuv_rgb;
+use std::fs::File;
+use std::io::Cursor;
+use std::io::Read;
+use aom_av1_sys::decode::Av1Decoder;
+use cvt_yuv_rgb::*;
+fn main()->Result<(), ()>{
+    let mut avif_file = File::open("./hato.profile0.8bpc.yuv420.avif").unwrap();
+    let mut avif_data  = Vec::new();
+    avif_file.read_to_end(&mut avif_data).map_err(|err|{ eprintln!("{:?}", err); return ();})?;
+    let avif = avif_parser::parse(Cursor::new(&mut avif_data))?;
+    println!("{:?}", avif);
+    let mut decoder:Av1Decoder = Av1Decoder::new().map_err(|err|{
+        eprintln!("{:?}", err);
+        return ();
+    })?;
+    if let Some(item) = avif.media_boxes().first(){
+        let tmp = &avif_data.as_slice()[item.0 as usize .. (item.0 + item.1) as usize];
+        eprintln!("{}\n{}\n{}", tmp.len(), item.1, item.1 == tmp.len() as u32);
+        decoder.decode(tmp).map_err(|err|{
+            eprintln!("{:?}", err);
+            return ();
+        })?;
+        let image = decoder.get_frame().ok_or(())?;
+        let mut bmp_image = bmp::Image::new(image.d_width(), image.d_height());
+        let yuv = YUVImageBuilder::new()
+        .plane_y(image.y_plane(), image.y_stride())
+        .plane_u(image.u_plane(), image.u_stride())
+        .plane_v(image.v_plane(), image.v_stride())
+        .depth(image.depth() as u32)
+        .width(image.d_width())
+        .height(image.d_height())
+        .chroma_shift_x(image.x_chroma_shift())
+        .chroma_shift_y(image.y_chroma_shift())
+        .build();
+        let color_prime = match image.color_primary(){
+            aom_av1_sys::AomColorPrimaries::BT2020=>cvt_yuv_rgb::color_primaries::BT2020,
+            aom_av1_sys::AomColorPrimaries::BT601=>cvt_yuv_rgb::color_primaries::BT601,
+            aom_av1_sys::AomColorPrimaries::BT709=>cvt_yuv_rgb::color_primaries::BT709,
+            _=>{
+                unimplemented!();
+            }
+        };
+        for (index, color) in RGB24Writer::new(YUV2RGB::new(yuv.iter(), color_prime)).enumerate(){
+            let index= index as u32;
+            let pixel = bmp::Pixel::new(color[0],color[1],color[2]);
+            bmp_image.set_pixel(index % image.d_width(), index / image.d_width(),  pixel);
+        }
+        bmp_image.save("./test-result.bmp").map_err(|err|{
+            eprintln!("{:?}", err);
+            return ();
+        });
+    }
+    
     println!("Hello, world!");
+    return Ok(());
 }
